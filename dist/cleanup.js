cf4fd0a115b6b87d803301b8a80e3ec64c315464
The Milk Man <ajay.ahir@red-gate.com>
2026-03-20 12:37:19 +0000

    rebuild

--- dist/cleanup.js
+++ dist/cleanup.js
@@ -1,5 +1,2 @@
-import { createRequire as e } from "node:module";
-import { dirname as r } from "node:path";
-import { fileURLToPath as i } from "node:url";
-const s = e(import.meta.url), a = i(import.meta.url), _ = r(a);
 process.argv[1].endsWith("cleanup.js");
+export {};
